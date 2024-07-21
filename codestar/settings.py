c83685adb454ac54d58f4207c361fbcbb0ef7170
laura fox <laurafox91@gmail.com>
2024-07-21 15:56:32 +0000

    meta ordering added to models

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-lauramayfox-djangoblog-7llrncxjt7z.ws-eu115.gitpod.io", 
 ".herokuapp.com"]
