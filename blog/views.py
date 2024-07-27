044fa282ce05b499900ae0a75d9c9ccd80decb1e
laura fox <laurafox91@gmail.com>
2024-07-27 22:57:58 +0000

    about app added and initiated

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,17 @@ class PostList(generic.ListView):
         template_name = "blog/index.html"
         paginate_by = 6
 
+def event_detail(request, event_id):
+    
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
+    )
+
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
@@ -30,4 +41,6 @@ def post_detail(request, slug):
         request,
         "blog/post_detail.html",
         {"post": post},
+        
+
     )
